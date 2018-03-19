6750661343dbeb1f00017e0543db26c9301bcee3
Ari Liikamaa <ari.liikamaa@gmail.com>
2018-03-20 00:19:50 +0200

    Project on entering s1l9

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	GameObject player;
+	float playerXPos, playerZPos;
+
+	// Use this for initialization
+	void Start () {
+	player = GameObject.FindGameObjectWithTag("Player");
+		
+	}
+
+	void LateUpdate () {
+
+	playerXPos = player.transform.position.x;
+	playerZPos= player.transform.position.z;
+	Vector3 ArmPos = new Vector3(playerXPos, 0.0f, playerZPos);
+	gameObject.transform.position = ArmPos;
+		
+	}
+}
