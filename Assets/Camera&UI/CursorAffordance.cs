70187ec61703bcc19acef7f31e80beeb3040e2a3
Ari Liikamaa <ari.liikamaa@gmail.com>
2018-03-20 23:30:27 +0200

    Moving to lecture 20. All working as supposed

--- Assets/Camera&UI/CursorAffordance.cs
+++ Assets/Camera&UI/CursorAffordance.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent (typeof(CameraRaycaster))]
 public class CursorAffordance : MonoBehaviour {
+
+	[SerializeField] Texture2D walkCursor = null;
+	[SerializeField] Texture2D targetCursor = null;
+	[SerializeField] Texture2D unknownCursor = null;
+	[SerializeField] Vector2 cursoHotspot = new Vector2(0,0);
 
 	CameraRaycaster camRaycaster;
 
@@ -11,7 +16,20 @@ public class CursorAffordance : MonoBehaviour {
 
 	}
 
-	void Update () {
-//		print(camRaycaster.layerHit);
+	void LateUpdate () {
+		switch (camRaycaster.currentLayerHit) {
+			case Layer.Walkable:
+				Cursor.SetCursor(walkCursor, cursoHotspot, CursorMode.Auto);
+				break;
+			case Layer.Enemy:
+				Cursor.SetCursor(targetCursor, cursoHotspot, CursorMode.Auto);
+				break;
+			case Layer.RaycastEndStop:
+				Cursor.SetCursor(unknownCursor, cursoHotspot, CursorMode.Auto);
+				break;
+			default:
+				Debug.LogError("What cursor to use here?");
+				return;
+			}
 	}
 }
--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -7,23 +7,32 @@ public class PlayerMovement : MonoBehaviour
 {
 	[SerializeField] float walkMoveStopRadius = 0.2f;
 
-    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    ThirdPersonCharacter thirdPersonCharacter;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster camRaycaster;
     Vector3 currentClickTarget;
+
+    bool isInDirectMode = false;
         
     private void Start()
     {
         camRaycaster = Camera.main.GetComponent<CameraRaycaster>();
-        m_Character = GetComponent<ThirdPersonCharacter>();
+        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
         currentClickTarget = transform.position;
     }
+	private void ProcessDirectMovement ()
+	{
+			float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+			Vector3 camForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+            Vector3 movement = v*camForward + h*Camera.main.transform.right;
 
-    // Fixed update is called in sync with physics
-    private void FixedUpdate ()
+			thirdPersonCharacter.Move (movement, false, false);
+	}
+	private void ProcressMouseMovement () // is called in FixedUpdate
 	{
 		if (Input.GetMouseButton (0)) {
-			print ("Cursor raycast hit" + camRaycaster.layerHit);
-			switch (camRaycaster.layerHit) {
+			print ("Cursor raycast hit" + camRaycaster.currentLayerHit);
+			switch (camRaycaster.currentLayerHit) {
 			case Layer.Walkable:
 				currentClickTarget = camRaycaster.hit.point;
 				break;
@@ -37,12 +46,27 @@ public class PlayerMovement : MonoBehaviour
 		}
 		var playerToClickPoint = currentClickTarget - transform.position;
 		if (playerToClickPoint.magnitude >= walkMoveStopRadius) {
-			m_Character.Move (currentClickTarget - transform.position, false, false);
-		} else 
-		{
-			m_Character.Move (Vector3.zero,false,false);
+			thirdPersonCharacter.Move (currentClickTarget - transform.position, false, false);
+		}
+		else {
+			thirdPersonCharacter.Move (Vector3.zero, false, false);
+		}
+	}
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate ()
+	{
+		if (Input.GetKeyDown (KeyCode.G)) {
+			isInDirectMode = !isInDirectMode;
+			currentClickTarget = transform.position;
 		}
 
+		if (isInDirectMode) {
+			ProcessDirectMovement();
+		} else 
+		{
+			ProcressMouseMovement ();
+		}
     }
 }
 
