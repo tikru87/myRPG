7ed9ff9343c1c24e68f3615f3a5639c443ff1811
Ari Liikamaa <ari.liikamaa@gmail.com>
2018-03-20 21:40:56 +0200

    Starting lecture 15

---
+++ Assets/Camera&UI/CursorAffordance.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+	CameraRaycaster camRaycaster;
+
+	void Start () {
+	camRaycaster = GetComponentInChildren<CameraRaycaster>();
+
+	}
+
+	void Update () {
+//		print(camRaycaster.layerHit);
+	}
+}
